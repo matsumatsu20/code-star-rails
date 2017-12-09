bc85288851ada9843a9d7e652915b47d6685b55f
Yuki Matsuyama <matsumatsu20@gmail.com>
2017-12-09 18:37:45 +0900

    add generate great message for aws service

--- app/controllers/hello_page_controller.rb
+++ app/controllers/hello_page_controller.rb
@@ -1,4 +1,11 @@
 class HelloPageController < ApplicationController
   def hello
+    @great = enerate_great_aws_service('CodeStar')
+  end
+
+  private
+
+  def generate_great_aws_service(service_name)
+    service_name << 'is great!!!'
   end
 end
